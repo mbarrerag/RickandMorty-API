0709176669ccd5290064863234845b03631029ab
MillerBarrera <mbarrerag@unal.edu.co>
2023-12-15 01:35:33 -0500

    Upgrade homepage, lets go baby

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FormSearchComponent } from './shared/components/form-search/form-search.component';
+import { PanelComponent } from './shared/components/panel/panel.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormSearchComponent,
+    PanelComponent
+  
   ],
   imports: [
     BrowserModule,
