85be3e25e8c9f3b88c5d7330bda9840c13a7d7ba
MillerBarrera <mbarrerag@unal.edu.co>
2023-12-15 01:46:04 -0500

    Update panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
+
 import { FormSearchComponent } from './shared/components/form-search/form-search.component';
 import { PanelComponent } from './shared/components/panel/panel.component';
 
@@ -12,7 +12,6 @@ import { PanelComponent } from './shared/components/panel/panel.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FormSearchComponent,
     PanelComponent
   
