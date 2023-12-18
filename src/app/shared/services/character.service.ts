9e56c20126ba872dab6a44f4f2464f90714b2bfc
MillerBarrera <mbarrerag@unal.edu.co>
2023-12-18 11:49:28 -0500

    Update Services request Http

--- src/app/shared/services/character.service.ts
+++ src/app/shared/services/character.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Character } from '../components/interface/character.interface';
+ import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,12 @@ import { Character } from '../components/interface/character.interface';
 export class CharacterService {
 
   constructor(private http: HttpClient) { }
-
+  
   searchCharacter(query ="",page = 1) {
-    return this.http.get<Character[]>('${environment.baseUrlAPI}/?name=${query}&page=${page}')
+    return this.http.get<Character[]>(`${environment.baseUrlAPI}/?name=${query}&page=${page}`)
 
   }
   getDetails(id: number) {
-    return this.http.get<Character[]>('${environment.baseUrlAPI}/${id}')
+    return this.http.get<Character[]>(`${environment.baseUrlAPI}/${id}`)
   }
 }
