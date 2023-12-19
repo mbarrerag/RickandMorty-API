f7f74b8e0bcf633b6e8749c1ebf775623f26d253
MillerBarrera <mbarrerag@unal.edu.co>
2023-12-19 11:57:33 -0500

    Lets goo Baby

--- src/app/shared/components/panel/panel.component.ts
+++ src/app/shared/components/panel/panel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,8 +12,9 @@ export class PanelComponent {
   tittle = 'https://vignette.wikia.nocookie.net/fictionalcrossover/images/c/c8/Rick_and_Morty_logo.png/revision/latest?cb=20150515125959';
   rick = 'https://media.tenor.com/H2mK_NOHBh8AAAAi/rick-and.gif'
   portal ='https://media.tenor.com/BgR83Df82t0AAAAi/portal-rick-and-morty.gif'
+  game = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1055214/Rick-Spaceship.png'
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private el: ElementRef, private renderer: Renderer2) { }
   onSearch(value: string) {
 
     console.log(value);
@@ -22,4 +23,40 @@ export class PanelComponent {
      }
       
     }
-}
+
+    private isMouseOver = false;
+
+   
+  
+    onMouseEnter() {
+      this.isMouseOver = true;
+    }
+  
+    onMouseLeave() {
+      this.isMouseOver = false;
+    }
+  
+    onMouseMove(event: MouseEvent) {
+      const imgElement = this.el.nativeElement.querySelector('.game');
+      const rect = imgElement.getBoundingClientRect();
+
+      // Calculate the percentage of mouse position within the image
+      const percentX = (event.clientX - rect.left) / rect.width;
+      const percentY = (event.clientY - rect.top) / rect.height;
+
+      // Use translate and percentage values for flexibility
+      this.renderer.setStyle(
+        imgElement,
+        'transform',
+        `translate(${percentX * 50}px, ${percentY * 50}px)`
+      );
+    }
+
+
+
+
+
+    }
+
+    
+
