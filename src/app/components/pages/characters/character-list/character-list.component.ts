cec6acf8c431a7930a8f988c246b51cff9c79cbd
MillerBarrera <mbarrerag@unal.edu.co>
2023-12-19 00:31:37 -0500

    Cards working

--- src/app/components/pages/characters/character-list/character-list.component.ts
+++ src/app/components/pages/characters/character-list/character-list.component.ts
@@ -23,6 +23,8 @@ export class CharacterListComponent {
   private query!: string;
   private hideScrollHeight = 200;
   private showScrollHeight = 500;
+  private maxValor = 10;
+
 
   constructor(private characterSvc: CharacterService) {}
     
@@ -35,11 +37,19 @@ export class CharacterListComponent {
           
         this.characterSvc.searchCharacter(this.query, this.pageNum).pipe(take(1)).subscribe((res: any) => {
         const { info, results } = res;
-   
-        this.characters = [...this.characters, res.results[1]];
-        this.info = info;
-        console.log(res.results);
+
         
+        this.characters = results.map((character: Character) => {
+          // Realizar la transformación deseada, si es necesario
+          // Por ejemplo, duplicar el valor de un atributo:
+          // character.attribute = character.attribute * 2;
+          return character;
+        });
+
+      
+        this.info = info;
+        
+        console.log(res.results);
         console.log("caracters",this.characters);
     });
   }
