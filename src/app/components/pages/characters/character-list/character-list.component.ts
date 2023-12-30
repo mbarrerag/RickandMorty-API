f3cb801b9bb65aa413d7cf04ad78adc55a4198ff
MillerBarrera <mbarrerag@unal.edu.co>
2023-12-30 11:46:58 -0500

    Fixed scroll infite

--- src/app/components/pages/characters/character-list/character-list.component.ts
+++ src/app/components/pages/characters/character-list/character-list.component.ts
@@ -1,6 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, HostListener, Inject } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, ParamMap, Params, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Params, Router } from '@angular/router';
 import { Character } from '@app/shared/components/interface/character.interface';
 import { CharacterService } from '@app/shared/services/character.service';
 import { filter, take } from 'rxjs/operators';
@@ -11,13 +11,11 @@ type RequestInfo = {
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.scss']
+  styleUrls: ['./character-list.component.scss'],
 })
 export class CharacterListComponent {
-
   info: RequestInfo = {
-
-    next: "",
+    next: '',
   };
 
   characters: Character[] = [];
@@ -26,61 +24,90 @@ export class CharacterListComponent {
   private query!: string;
   private hideScrollHeight = 200;
   private showScrollHeight = 500;
-    backGroundImages = 'https://wallpapercave.com/wp/Y0StpXn.jpg';
+  backGroundImages = 'https://wallpapercave.com/wp/Y0StpXn.jpg';
 
-
-  constructor(private router: Router, private characterSvc: CharacterService, private route: ActivatedRoute, @Inject(DOCUMENT) private document: Document) {
+  constructor(
+    private router: Router,
+    private characterSvc: CharacterService,
+    private route: ActivatedRoute,
+    @Inject(DOCUMENT) private document: Document
+  ) {
     this.OnUrlChange();
-   }
+  }
 
   private OnUrlChange() {
-   this.router.events.pipe((filter((event) => event instanceof NavigationEnd))).subscribe(() => {
-     this.characters = [];
-      this.pageNum = 1;
-      this.getCharactersByQuery
-   });
- }
-
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.characters = [];
+        this.pageNum = 1;
+        this.getCharactersByQuery();
+      });
+  }
 
   onSearch(value: string) {
-
     console.log(value);
     if (value && value.length > 3) {
       this.router.navigate(['/character-list'], { queryParams: { q: value } });
     }
-
   }
-  ngOnInit(): void {
 
+  ngOnInit(): void {
     this.getCharactersByQuery();
   }
 
   @HostListener('window:scroll', [])
-  onWindowScroll():void {
-    const yOffSet = window.pageYOffset
-    if ((yOffSet || this.document.documentElement.scrollTop || this.document.body.scrollTop) > this.showScrollHeight) {
+  onWindowScroll(): void {
+    const yOffSet = window.pageYOffset;
+    if (
+      (yOffSet ||
+        this.document.documentElement.scrollTop ||
+        this.document.body.scrollTop) > this.showScrollHeight
+    ) {
       this.showGoUpButton = true;
-    } else if (this.showGoUpButton && (yOffSet || this.document.documentElement.scrollTop || this.document.body.scrollTop) < this.hideScrollHeight) {
+    } else if (
+      this.showGoUpButton &&
+      (yOffSet ||
+        this.document.documentElement.scrollTop ||
+        this.document.body.scrollTop) < this.hideScrollHeight
+    ) {
       this.showGoUpButton = false;
+    }
+
+    // Manually trigger onScrollDown when reaching the bottom of the page
+    const windowHeight =
+      'innerHeight' in window
+        ? window.innerHeight
+        : document.documentElement.offsetHeight;
+    const body = document.body,
+      html = document.documentElement;
+    const docHeight = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+    const windowBottom = windowHeight + window.pageYOffset;
+
+    if (windowBottom >= docHeight - 200) {
+      this.onScrollDown();
     }
   }
 
-  
-
-  onScrollDown():void{
+  onScrollDown(): void {
     if (this.info.next) {
       this.pageNum++;
       this.getDataFromService();
-      console.log("Number of pages",this.pageNum);
-  
+      console.log('Number of pages', this.pageNum);
     }
   }
 
-  onScrollTop():void{
+  onScrollTop(): void {
     this.document.body.scrollTop = 0; // Safari
     this.document.documentElement.scrollTop = 0; // Other
   }
- 
+
   private getCharactersByQuery(): void {
     this.route.queryParams.pipe(take(1)).subscribe((params: Params) => {
       console.log('Params->', params);
@@ -89,21 +116,24 @@ export class CharacterListComponent {
     });
   }
 
-
- 
   private getDataFromService(): void {
-    this.characterSvc.searchCharacter(this.query, this.pageNum).pipe(take(1)).subscribe((res: any) => {
-      const { info, results } = res;
-      
-      if (res?.results?.length) {
-        // Append new characters to the existing array
-        this.characters = [...this.characters, ...results.map((character: Character) => character)];
-        this.info = info;
-      } else {
-        // Handle the case when there are no results
-        this.characters = [];
-      }
-    });
+    this.characterSvc
+      .searchCharacter(this.query, this.pageNum)
+      .pipe(take(1))
+      .subscribe((res: any) => {
+        const { info, results } = res;
+
+        if (res?.results?.length) {
+          // Append new characters to the existing array
+          this.characters = [
+            ...this.characters,
+            ...results.map((character: Character) => character),
+          ];
+          this.info = info;
+        } else {
+          // Handle the case when there are no results
+          this.characters = [];
+        }
+      });
   }
-  
 }
